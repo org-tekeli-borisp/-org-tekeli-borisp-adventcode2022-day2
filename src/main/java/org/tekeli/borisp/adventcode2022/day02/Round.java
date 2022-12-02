0000df4f17f7cee5ba7f2572910cecf384abd724
borisp <boris.pigin@gmail.com>
2022-12-02 21:54:21 +0100

    Round: introduction

---
+++ src/main/java/org/tekeli/borisp/adventcode2022/day02/Round.java
@@ -0,0 +1,11 @@
+package org.tekeli.borisp.adventcode2022.day02;
+
+public class Round {
+    private final Move first;
+    private final Move second;
+
+    public Round(final Move first, final Move second) {
+        this.first = first;
+        this.second = second;
+    }
+}
